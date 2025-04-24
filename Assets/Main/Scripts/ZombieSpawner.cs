beb56759138310209fcdfe1385f15475f133a864
Dantetisso <dantetisoriero@gmail.com>
2025-04-24 18:22:53 -0300

    oleada super simple

--- Assets/Main/Scripts/ZombieSpawner.cs
+++ Assets/Main/Scripts/ZombieSpawner.cs
@@ -7,10 +7,36 @@ public class ZombieSpawner : MonoBehaviour
 {
     [SerializeField] private Transform[] spawnPoints;
     [SerializeField] private GameObject zombiePrefab;
+    [SerializeField] private float maxTime;
+    [SerializeField] private float currentTime;
+    [SerializeField] private int maxWaves;
+    [SerializeField] private int waveCount;
+    private bool canSpawn = true;
+
+    // "sistema de oleadas" super simple 
 
     void Start()
     {
-        Spawn();
+       // Spawn();
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (canSpawn)
+        {
+            if (currentTime >= maxTime)
+            {
+                Spawn();
+                currentTime = 0;
+            }
+        }
+
+        if (waveCount == maxWaves)
+        {
+            canSpawn = false;
+        }
     }
 
     void Spawn()
@@ -19,5 +45,7 @@ public class ZombieSpawner : MonoBehaviour
         {
             Instantiate(zombiePrefab, spawnPoints[i].position, quaternion.identity);
         }
+
+        waveCount++;
     }
 }
