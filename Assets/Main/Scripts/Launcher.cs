b4d7c9da4af2314cdc9d15532f00d8d189414f2a
matmiguez <matinicomig@gmail.com>
2025-04-24 15:17:55 -0300

    todos los pasos del PUN 2

---
+++ Assets/Main/Scripts/Launcher.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace zombieonlinegame
+{
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+
+        [SerializeField] private byte maxPlayersPerRoom = 4;
+
+        [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+        [SerializeField] private GameObject controlPanel;
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        [SerializeField] private GameObject progressLabel;
+        /// <summary>
+        /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
+        /// </summary>
+        string gameVersion = "1";
+
+
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+        /// </summary>
+        void Awake()
+        {
+            // #Critical
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+
+        public void Connect()
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+            if (PhotonNetwork.IsConnected)
+            {
+                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                // #Critical, we must first and foremost connect to Photon Online Server.
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+            }
+        }
+
+
+        public override void OnConnectedToMaster()
+        {
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
+            Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
+    }
+}
