73807c186348db8316fdf2d2319d0a895a478380
Dantetisso <dantetisoriero@gmail.com>
2025-07-07 17:48:29 -0300

    cambio nombre de script para mejor claridad
    ya que ese script esta sincronizando el gunholder con el photon (cambio de armas y feedbacks)

--- Assets/Main/Scripts/PlayerGunSync.cs
+++ Assets/Main/Scripts/PlayerGunSync.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class PlayerGunFeedback : MonoBehaviour // este script es para que se puedan dar los rpc de las armas
-{                                               // asi no se tiene que poner mas photon view a las armas, solo usan las del player
+public class PlayerGunSync : MonoBehaviourPun // este script es para que se puedan dar los rpc de las armas, es como un puente entre el gunholder y photon
+{                                               // asi no se tiene que poner mas photon view a las armas, solo usa el photonview del player
     [SerializeField] private GunHolderScript gunHolder;
 
     [PunRPC]
